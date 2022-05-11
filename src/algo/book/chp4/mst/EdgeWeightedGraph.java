369da5e77391ebbb86eae4f1dd7e7a09a0bde2ae
Rafael Freire <rafaelcfreire@gmail.com>
2022-05-11 09:20:24 -0500

    MST Prims algorithm

--- src/algo/book/chp4/mst/EdgeWeightedGraph.java
+++ src/algo/book/chp4/mst/EdgeWeightedGraph.java
@@ -1,11 +1,41 @@
 package algo.book.chp4.mst;
 
 import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+
+import java.util.NoSuchElementException;
 
 public class EdgeWeightedGraph {
     private final int V;
     private int E;
     private Bag<Edge>[] adj;
+
+    public EdgeWeightedGraph(In in) {
+        if (in == null) throw new IllegalArgumentException("argument is null");
+
+        try {
+            V = in.readInt();
+            adj = (Bag<Edge>[]) new Bag[V];
+            for (int v = 0; v < V; v++) {
+                adj[v] = new Bag<Edge>();
+            }
+
+            int E = in.readInt();
+            if (E < 0) throw new IllegalArgumentException("Number of edges must be non-negative");
+            for (int i = 0; i < E; i++) {
+                int v = in.readInt();
+                int w = in.readInt();
+                validateVertex(v);
+                validateVertex(w);
+                double weight = in.readDouble();
+                Edge e = new Edge(v, w, weight);
+                addEdge(e);
+            }
+        }
+        catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("invalid input format in EdgeWeightedGraph constructor", e);
+        }
+    }
 
     public EdgeWeightedGraph(int V) {
         this.V = V;
@@ -13,6 +43,11 @@ public class EdgeWeightedGraph {
         adj = (Bag<Edge>[]) new Bag[V];
         for (int v = 0; v < V; v++)
             adj[v] = new Bag<Edge>();
+    }
+
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
     }
 
     public int V() {
