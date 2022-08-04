e39e708ceed1a95ae7b3cda5d59ee031f1f2888b
Rafael Freire <rfreire@corelogic.com>
2022-08-04 13:02:31 -0500

    Radix sort and wee 7 of coursera course

--- src/algoexpert/sorting/RadixSort.java
+++ src/algoexpert/sorting/RadixSort.java
@@ -1,23 +1,46 @@
 package algoexpert.sorting;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class RadixSort {
     public static void main(String[] args) {
-        radixSort(List.of(8762, 654, 3008, 345, 87, 65, 234, 12, 2));
+        radixSort(Arrays.asList(8762, 654, 3008, 345, 87, 65, 234, 12, 2));
     }
 
-    public static int[] radixSort(List<Integer> array) {
+    // (int) (curr / Math.pow(10, 5)) % 10
+    public static List<Integer> radixSort(List<Integer> array) {
+        int max = Collections.max(array);
+
+        int digit = 0;
+        while ((max / Math.pow(10, digit)) > 0) {
+            sort(array, digit);
+            digit += 1;
+        }
+        return array;
+    }
+
+    private static void sort(List<Integer> array, int digit) {
         int n = array.size();
         int R = 10;
         int[] aux = new int[n];
-        int length = (int)(Math.log10(Collections.max(array))+1);
+        int[] count = new int[R + 1];
 
-        for (int d = length; d >= 0; d--) {
-
+        for (int i = 0; i < n; i++) {
+            int value = (int)(array.get(i) / Math.pow(10, digit)) % 10;
+            count[value + 1]++;
         }
-        return aux;
+
+        for (int i = 0; i < R; i++)
+            count[i + 1] += count[i];
+
+        for (int i = 0; i < n; i++) {
+            int temp = (int)(array.get(i) / Math.pow(10, digit)) % 10;
+            aux[count[temp]++] = array.get(i);
+        }
+
+        for (int i = 0; i < n; i++)
+            array.set(i, aux[i]);
     }
 }
