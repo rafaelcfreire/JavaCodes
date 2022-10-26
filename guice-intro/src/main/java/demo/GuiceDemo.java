2dfd1b77ff30ed6ca35e39f8815c68de08fdce31
Rafael Freire <rafaelcfreire@gmail.com>
2022-10-26 14:00:01 -0500

    Guice Demo

--- guice-intro/src/main/java/demo/GuiceDemo.java
+++ guice-intro/src/main/java/demo/GuiceDemo.java
@@ -1,9 +1,6 @@
 package demo;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.google.inject.Provides;
+import com.google.inject.*;
 
 import javax.inject.Inject;
 import javax.inject.Qualifier;
@@ -18,6 +15,10 @@ public class GuiceDemo {
     @Qualifier
     @Retention(RetentionPolicy.RUNTIME)
     @interface Count {}
+
+    @Qualifier
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Text {}
 
     static class DemoModule extends AbstractModule {
 //        @Provides
@@ -34,23 +35,32 @@ public class GuiceDemo {
 
         @Override
         protected void configure() {
-//            bind()
+            bind(String.class).annotatedWith(Message.class).toInstance("Hello World");
+            bind(String.class).annotatedWith(Text.class).toInstance("Another text");
+            bind(Integer.class).toInstance(3);
+
         }
     }
 
     static class Greeter {
+
+        @Message
         private final String message;
         private final int count;
 
+        @Text
+        private final String text;
+
         @Inject
-        Greeter(@Message String message, @Count int count) {
+        Greeter(@Message String message, int count, @Text String text) {
             this.message = message;
             this.count = count;
+            this.text = text;
         }
 
         void sayHello() {
             for (int i = 0; i < count; i++) {
-                System.out.println(message);
+                System.out.println(message+", "+text);
             }
         }
     }
